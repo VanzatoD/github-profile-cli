b5a0c22d24a18307b25dad014be181df76a06ba6
Daniel <daniel.vanzato2@gmail.com>
2026-04-25 01:41:54 -0300

    refactor: deleting unused lines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+# import os
 
 login = input("GitHub user: ")
 
@@ -23,17 +24,6 @@ if res.status_code == 200:
         "followers": [],
         "following": []
     }
-    
-    # 
-    # print("Name:", data.get("name") or "Anonymous")
-    # print("Username: ", data.get("login"))
-    # print("Email: ", data.get("email") or "Private")
-    # print("Bio: ", data.get("bio") or "no bio")
-    # print("Public repos: ", data.get("public_repos") or 0)
-    # print("Followers: ", data.get("followers") or 0)
-    # print("Following: ", data.get("following") or 0)
-
-    # 
 
     repos_res = requests.get(data["repos_url"])
     
@@ -50,15 +40,13 @@ if res.status_code == 200:
         }
         output["repos"].append(repo_data)
 
-        # print("\n Name: ", repo.get("name"))
-        # print("Language: ", repo.get("language") or "N/A")
-        # print("Stars: ", repo.get("stargazers_count") or 0)
-    
+# saving output_{login}.json:
     filename = f"output_{login}.json"
 
     with open(filename, "w") as f:
         json.dump(output, f, indent=4)
-    
+
+# printing results:
     print("\n === PROFILE ===")
     for key, value in output["profile"].items():
         print(f"{key}: {value}")
