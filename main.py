3cb530394c0f0e3cc9c02d7ba5b63dcf98639aa2
Daniel <daniel.vanzato2@gmail.com>
2026-04-25 01:22:53 -0300

    refactor:print direct from generated json file

--- main.py
+++ main.py
@@ -5,52 +5,70 @@ login = input("GitHub user: ")
 
 url = f"https://api.github.com/users/{login}"
 res = requests.get(url)
+print(res)
 
 if res.status_code == 200:
     data = res.json()
     output = {
         "profile": {
-            "login": data.get("login"),
-            "name": data.get("name"),
-            "email": data.get("email"),
-            "bio": data.get("bio"),
-            "public_repos": data.get("public_repos"),
-            "followers": data.get("followers"),
-            "following": data.get("following"),
+            "username": data.get("login"),
+            "name": data.get("name") or "Anonymous",
+            "email": data.get("email") or "Private",
+            "bio": data.get("bio") or "No bio",
+            "public_repos": data.get("public_repos") or 0,
+            "followers": data.get("followers")or 0,
+            "following": data.get("following")or 0
         },
         "repos": [],
         "followers": [],
         "following": []
     }
     
-    print("\n === PROFILE ===")
-    print("Name:", data.get("name") or "Anonymous")
-    print("Username: ", data.get("login"))
-    print("Email: ", data.get("email") or "Private")
-    print("Bio: ", data.get("bio") or "no bio")
-    print("Public repos: ", data.get("public_repos") or 0)
-    print("Followers: ", data.get("followers") or 0)
-    print("Following: ", data.get("following") or 0)
+    # 
+    # print("Name:", data.get("name") or "Anonymous")
+    # print("Username: ", data.get("login"))
+    # print("Email: ", data.get("email") or "Private")
+    # print("Bio: ", data.get("bio") or "no bio")
+    # print("Public repos: ", data.get("public_repos") or 0)
+    # print("Followers: ", data.get("followers") or 0)
+    # print("Following: ", data.get("following") or 0)
 
-    print("\n === REPOSITORIES ===")
+    # 
 
-    repos = requests.get(data["repos_url"]).json()
+    repos_res = requests.get(data["repos_url"])
+    
+    if repos_res.status_code == 200:
+        repos = repos_res.json()
+    else:
+            repos = []
 
     for repo in repos:
-        output["repos"].append({
+        repo_data = {
             "name": repo.get("name"),
             "language": repo.get("language"),
             "stars": repo.get("stargazers_count")
-        })
+        }
+        output["repos"].append(repo_data)
 
-        print("\n Name: ", repo.get("name"))
-        print("Language: ", repo.get("language") or "N/A")
-        print("Stars: ", repo.get("stargazers_count") or 0)
+        # print("\n Name: ", repo.get("name"))
+        # print("Language: ", repo.get("language") or "N/A")
+        # print("Stars: ", repo.get("stargazers_count") or 0)
     
     filename = f"output_{login}.json"
 
     with open(filename, "w") as f:
         json.dump(output, f, indent=4)
+    
+    print("\n === PROFILE ===")
+    for key, value in output["profile"].items():
+        print(f"{key}: {value}")
+
+    print("\n === REPOSITORIES ===")
+    for repo in output["repos"]:
+        for key, value in repo.items():
+            print(f"{key}: {value}")
+        print("---")
+
     print(f"\nSaved to {filename}")
 
 else:
